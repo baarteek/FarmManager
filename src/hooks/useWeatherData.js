10ff73a6452fcba0a830fbf41731f48229cac1fe
baarteek <bartekkrukowski822@gmail.com>
2024-04-11 21:35:14 +0200

    add a hook that fetchs weather inforation

---
+++ src/hooks/useWeatherData.js
@@ -0,0 +1,35 @@
+import { useState, useEffect } from 'react';
+
+const useWeatherData = (latitude, longitude) => {
+    const [weatherData, setWeatherData] = useState({ current: null, forecast: null });
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+
+    useEffect(() => {
+        const fetchWeatherData = async () => {
+            try {
+                const url = `https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current=temperature_2m,relative_humidity_2m,weather_code,wind_speed_10m,wind_direction_10m&hourly=temperature_2m,relative_humidity_2m,dew_point_2m,precipitation_probability,weather_code,visibility,wind_speed_10m,wind_direction_10m,soil_temperature_0cm,soil_temperature_6cm,soil_temperature_18cm,soil_moisture_0_to_1cm,soil_moisture_1_to_3cm,soil_moisture_3_to_9cm,soil_moisture_9_to_27cm&daily=weather_code,temperature_2m_max,temperature_2m_min,precipitation_hours,precipitation_probability_max,wind_speed_10m_max&timezone=auto`;
+                const response = await fetch(url);
+                const data = await response.json();
+                setWeatherData({
+                    current: data.current,
+                    hourly: data.hourly,
+                    daily: data.daily
+                });
+            } catch (err) {
+                setError(err.message);
+            } finally {
+                setLoading(false);
+            }
+        };
+
+        if (latitude && longitude) {
+            fetchWeatherData();
+        }
+    }, [latitude, longitude]);
+
+    return { weatherData, loading, error };
+};
+
+export default useWeatherData;
--- src/screens/WeatherScreen.js
+++ src/screens/WeatherScreen.js
@@ -1,12 +1,27 @@
 import { ScrollView, Text, View } from "react-native";
 import { styles } from "../styles/AppStyles";
 import useLocation from "../hooks/useLocation";
+import useWeatherData from "../hooks/useWeatherData";
 
-const WeatherScreen = ({navigation}) => {
-    const {location, error} = useLocation();
+
+const WeatherScreen = () => {
+    const { location, locationEror } = useLocation();
+    const { weatherData, loading, error } = useWeatherData(location?.coords?.latitude, location?.coords?.longitude);
+
+    console.log(weatherData.daily);
 
     return (
             <ScrollView style={styles.mainCantainer}>
+                <View style={styles.container} >
+                    <View style={styles.topContainer}>
+                        <Text style={styles.title}>Current Weather</Text>
+                    </View>
+                    <View style={styles.bottomContainer} >
+                        <View style={styles.content} >
+                            <Text style={styles.text}>Content</Text>
+                        </View>
+                    </View>
+                </View>
                 <View style={styles.container} >
                     <View style={styles.topContainer}>
                         <Text style={styles.title}>Today's Weather</Text>
@@ -26,7 +41,7 @@ const WeatherScreen = ({navigation}) => {
                             {location && (
                                 <Text style={styles.text}>Latitude: {location.coords.latitude}, Longitude: {location.coords.longitude}</Text>
                             )}
-                            {error && <Text style={styles.text}>{error}</Text>}
+                            {locationEror && <Text style={styles.text}>{locationEror}</Text>}
                         </View>
                     </View>
                 </View>
