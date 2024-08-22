fea03b0c4fb36f34e1019457b6cebea292692b0c
Bartek Krukowski <bartekkrukowski822@gmail.com>
2024-08-22 12:20:12 +0200

    add display of Soil Measurements and Plot Numbers in FieldDetails

--- src/components/FieldDetails.js
+++ src/components/FieldDetails.js
@@ -3,23 +3,10 @@ import { View, Text, TouchableOpacity, Alert } from "react-native";
 import ExpandableComponent from "./ExpandableComponent";
 import { useNavigation } from '@react-navigation/native';
 import { styles } from '../styles/AppStyles';
-import { useFieldContext } from '../context/FieldProvider';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
 const FieldDetails = ({ fieldData, onDelete }) => {
     const navigation = useNavigation();
-    const { deleteSoilMeasurement } = useFieldContext();
-
-    const handleDeleteMeasurement = (fieldId, measurementIndex) => {
-        Alert.alert(
-            "Confirm Deletion",
-            "Are you sure you want to delete this soil measurement?",
-            [
-                { text: "Cancel", style: "cancel" },
-                { text: "Delete", onPress: () => deleteSoilMeasurement(fieldId, measurementIndex), style: "destructive" }
-            ],
-            { cancelable: false }
-        );
-    };
 
     const parseDate = (dateString) => {
         const [day, month, year] = dateString.split('.').map(part => parseInt(part, 10));
@@ -48,32 +35,15 @@ const FieldDetails = ({ fieldData, onDelete }) => {
                         sortedSoilMeasurements && sortedSoilMeasurements.length > 0 ? (
                             sortedSoilMeasurements.map((measurement, index) => (
                                 <React.Fragment key={index}>
-                                    <Text style={styles.title}>{measurement.date}</Text>
-                                    <View style={styles.infoRowContainer}>
-                                        <Text style={styles.text}>pH:</Text>
-                                        <Text style={styles.text}>{measurement.pH}</Text>
-                                    </View>
-                                    <View style={[styles.line, { borderColor: '#DFF6DF' }]} />
-                                    <View style={styles.infoRowContainer}>
-                                        <Text style={styles.text}>Nitrogen:</Text>
-                                        <Text style={styles.text}>{measurement.nitrogen}</Text>
-                                    </View>
-                                    <View style={[styles.line, { borderColor: '#DFF6DF' }]} />
-                                    <View style={styles.infoRowContainer}>
-                                        <Text style={styles.text}>Phosphorus:</Text>
-                                        <Text style={styles.text}>{measurement.phosphorus}</Text>
-                                    </View>
-                                    <View style={[styles.line, { borderColor: '#DFF6DF' }]} />
-                                    <View style={styles.infoRowContainer}>
-                                        <Text style={styles.text}>Potassium:</Text>
-                                        <Text style={styles.text}>{measurement.potassium}</Text>
-                                    </View>
-                                    <View style={[styles.rowContainer, { justifyContent: 'space-around', marginTop: '10%' }]}>
-                                        <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('Edit Soil Measurement', { fieldId: fieldData.id, measurementIndex: measurement.originalIndex })}>
-                                            <Text style={{ textAlign: 'center', fontWeight: 'bold', fontSize: 16, marginLeft: '10%', marginRight: '10%' }}>Edit</Text>
+                                    <View style={styles.infoRowContainer} >
+                                        <TouchableOpacity style={{width: '70%'}}>
+                                            <Text style={styles.text}>{measurement.name}</Text>
                                         </TouchableOpacity>
-                                        <TouchableOpacity style={[styles.button, { backgroundColor: '#FC7F7F' }]} onPress={() => handleDeleteMeasurement(fieldData.id, measurement.originalIndex)}>
-                                            <Text style={{ textAlign: 'center', fontWeight: 'bold', fontSize: 16, color: '#fff', marginLeft: '10%', marginRight: '10%' }}>Delete</Text>
+                                        <TouchableOpacity>
+                                            <Icon name="edit" size={22} color="#00BFFF" />
+                                        </TouchableOpacity>
+                                        <TouchableOpacity>
+                                            <Icon name="delete" size={22} color="#FC7F7F" />
                                         </TouchableOpacity>
                                     </View>
                                     <View style={[styles.line, { borderColor: '#22734D', marginBottom: '5%', marginTop: '5%' }]} />
@@ -94,27 +64,42 @@ const FieldDetails = ({ fieldData, onDelete }) => {
                 </ExpandableComponent>
                 <ExpandableComponent title="Plot Numbers" isExpanded={false} backgroundColor="#BAF1BA" style={{ width: '100%' }}>
                     {
-                        fieldData.plotNumbers && fieldData.plotNumbers.length > 0 ? (
-                            fieldData.plotNumbers.map((plotNumber, index) => (
-                                <React.Fragment key={index}>
-                                    <Text style={[styles.text, { fontSize: 14 }]}>{plotNumber}</Text>
-                                    <View style={[styles.line, { borderColor: '#DFF6DF' }]} />
+                        fieldData.referenceParcels && fieldData.referenceParcels.length > 0 ? (
+                            fieldData.referenceParcels.map((referenceParcels) => (
+                                <React.Fragment key={referenceParcels.id}>
+                                    <View style={styles.infoRowContainer} >
+                                        <TouchableOpacity style={{width: '70%'}}>
+                                            <Text style={styles.text}>{referenceParcels.name}</Text>
+                                        </TouchableOpacity>
+                                        <TouchableOpacity>
+                                            <Icon name="edit" size={22} color="#00BFFF" />
+                                        </TouchableOpacity>
+                                        <TouchableOpacity>
+                                            <Icon name="delete" size={22} color="#FC7F7F" />
+                                        </TouchableOpacity>
+                                    </View>
+                                    <View style={[styles.line, { borderColor: '#22734D', marginBottom: '5%', marginTop: '5%' }]} />
                                 </React.Fragment>
                             ))
                         ) : (
                             <Text style={[styles.text, { textAlign: 'center' }]}>There are no plot numbers for this field</Text>
                         )
                     }
+                     <View style={[styles.rowContainer, { justifyContent: 'space-around' }]}>
+                        <TouchableOpacity style={[styles.button, { backgroundColor: '#00E000', width: '80%' }]} onPress={() => navigation.navigate('Add Plot Number', { fieldId: fieldData.id })}>
+                            <Text style={{ textAlign: 'center', fontWeight: 'bold', fontSize: 16, color: '#fff', marginLeft: '10%', marginRight: '10%' }}>Add</Text>
+                        </TouchableOpacity>
+                    </View>
                 </ExpandableComponent>
                 <TouchableOpacity style={[styles.button, { marginTop: '5%', paddingVertical: '1%', backgroundColor: '#BAF1BA' }]} onPress={() => navigation.navigate('Show Crops', { fieldId: fieldData.id })}>
                     <Text style={{ textAlign: 'center', fontWeight: 'bold', fontSize: 20, marginLeft: '10%', marginRight: '10%', color: '#22734D' }}>Show Crops</Text>
                 </TouchableOpacity>
                 <View style={[styles.rowContainer, { justifyContent: 'space-around', marginTop: '3%' }]}>
                     <TouchableOpacity style={[styles.button, { backgroundColor: '#00BFFF', width: '40%' }]}  onPress={() => navigation.navigate('Edit Field', { field: fieldData })}>
-                        <Text style={{ textAlign: 'center', fontWeight: 'bold', fontSize: 16, color: '#fff', marginHorizontal: 10 }}>Edit</Text>
+                        <Text style={{ textAlign: 'center', fontWeight: 'bold', fontSize: 16, color: '#fff', marginHorizontal: 10 }}>Edit Field</Text>
                     </TouchableOpacity>
                     <TouchableOpacity style={[styles.button, { backgroundColor: '#FC7F7F' }]} onPress={onDelete}>
-                        <Text style={{ textAlign: 'center', fontWeight: 'bold', fontSize: 16, color: '#fff', marginLeft: '10%', marginRight: '10%' }}>Delete</Text>
+                        <Text style={{ textAlign: 'center', fontWeight: 'bold', fontSize: 16, color: '#fff', marginLeft: '10%', marginRight: '10%' }}>Delete Field</Text>
                     </TouchableOpacity>
                 </View>
             </ExpandableComponent>
